ac0fabcd1d723b423b3fbddb95f771951e81222e
jiangmichael1 <jiangm1@yahoo.com>
2020-11-22 22:10:59 -0500

    Figured out display toggle

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
+const header = document.querySelector('.header');
 const navBar = document.querySelector('.navbar');
 const aboutSect = document.querySelector('.about_me');
+const landSect = document.querySelector('.landing');
 const workSect = document.querySelector('.work');
 const eduSect = document.querySelector('.education');
 const projSect = document.querySelector('.projects');
 const contSect = document.querySelector('.contact');
-const navBarSections = [aboutSect, workSect, eduSect, projSect, contSect]
+const navBarSections = [aboutSect, workSect, eduSect, projSect, contSect, landSect];
 
+const displayLanding = () => {
+    if (landSect.style.display !== "block"){
+        landSect.style.display = "block";
+    }
+}
 
+displayLanding();
+
+const toggleSection = (section) => {
+    let filteredSections = navBarSections.filter(sect => sect !== section)
+
+    filteredSections.forEach(sect => {
+        sect.style.display = 'none';
+    })
+
+    section.style.display = "block";
+}
+
+header.addEventListener('click', (e) => {
+    if (e.target.textContent === "Michael Jiang") {
+        toggleSection(landSect);
+    }
+})
 
 navBar.addEventListener('click', (e) => {
     if (e.target.className === "navbar--about"){
-        aboutSect.style.display = "block";
-        workSect.style.display = "none";
-        eduSect.style.display = "none";
-        projSect.style.display = "none";
-        contSect.style.display = "none";
+        toggleSection(aboutSect);
     } else if (e.target.className === "navbar--work") {
-        console.log("work!");
+        toggleSection(workSect);
     } else if (e.target.className === "navbar--education") {
-        console.log("education!")
+        toggleSection(eduSect);
     } else if (e.target.className === "navbar--projects") {
-        console.log("projects!");
+        toggleSection(projSect);
     } else if (e.target.className === "navbar--contact") {
-        console.log("contact!");
+        toggleSection(contSect);
     } 
 })
