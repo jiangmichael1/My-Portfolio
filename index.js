c7d9f209df7798492dba0155fad16319cabd6833
jiangmichael1 <jiangm1@yahoo.com>
2020-12-23 15:38:07 -0500

    Trying out new form style

--- index.js
+++ index.js
@@ -100,3 +100,22 @@ workNav.addEventListener('click', (e) => {
         toggleWorkText(mathText);
     }
 })
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
